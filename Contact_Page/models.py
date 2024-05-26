5c4d9b96c55c834a5eacba13cb4f0f1a702677d0
https://github.com/sifathossen48 <sifathossen967@gmail.com>
2024-05-26 11:30:51 +0600

    Contact Form Dynamic

--- Contact_Page/models.py
+++ Contact_Page/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=40)
+    email = models.EmailField(max_length=60)
+    subject = models.CharField(max_length=50)
+    message = models.TextField()
+    def __str__(self):
+        return self.name
--- Contact_Page/views.py
+++ Contact_Page/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.shortcuts import redirect, render
+from django.contrib import messages
+from Contact_Page import forms
 # Create your views here.
-class NewsView(TemplateView):
-    template_name = 'contact.html'
+def contact(request):
+    if request.method == 'POST':
+        form = forms.ContactForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your message has been sent. Thank you!') # Redirect to a success page
+        else:
+            # Print form errors to console for debugging
+            print(form.errors)
+            messages.error(request, 'Invalid! Please try again.')
+    else:
+        form = forms.ContactForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'contact.html', context)
